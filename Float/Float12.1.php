7f4a33a9334231886f8b8fa799b8454ec9fc83c1
iamsupergirl <alona.ant@bk.ru>
2017-07-05 01:29:51 +0300

    Задание "Танк" Янушкевич Алена

---
+++ Float/Float12.1.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: helen
+ * Date: 4.7.17
+ * Time: 22.05
+ */
+include 'Room.php';
+class Float12
+{
+    /**
+     * @var array
+     */
+    private $rooms;
+    public function __construct($rooms)
+    {
+        $this->rooms = [];
+    }
+
+    public function addRoom()
+    {
+        $this->rooms=new Room();
+
+    }
+
+    public function delRoom($i)
+    {
+          if (count($this->rooms[$i])<$i)
+                unset($this->rooms[$i]);
+
+
+    }
+        public function infoRoom()
+        {
+            $report='';
+            foreach ($this->rooms as $value){
+            $report=$report.$value->status;
+            }
+            return 'Количество комнат:' .count ($this->rooms).'Информация о комнате:'.$report;
+        }
+}
---
+++ Float/Room.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: helen
+ * Date: 4.7.17
+ * Time: 22.20
+ */
+class Room
+{
+    /**
+     * @var int
+     */
+    private $windows = 0;
+    /**
+     * @var bool
+     */
+    private $door = 0;
+    /**
+     * @var int
+     */
+    private $status = 0;
+    /**
+     * @var array
+     */
+    private $human;
+
+    /**
+     * @return array
+     */
+    public function getHuman()
+    {
+        return $this->human;
+    }
+
+    /**
+     * @param int $human
+     */
+    public function setHuman($human)
+    {
+        $this->human = $human;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getWindows()
+    {
+        return $this->windows;
+    }
+
+    /**
+     * @param int $windows
+     */
+    public function setWindows($windows)
+    {
+        $this->windows = $windows;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDoor()
+    {
+        return $this->door;
+    }
+
+    /**
+     * @param int $door
+     */
+    public function setDoor($door)
+    {
+        $this->door = $door;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function addHuman()
+    {
+        $this->human=new Human();
+        }
+
+    public function delHuman($a)
+    {
+        if ($this->door && count($this->human[$a])<$a) {
+            unset($this->human[$a]);
+        }
+
+    }
+    public function status(){
+        if (count($this->human)){
+            return'В комнате:'.count($this->human).'человек.Свет горит';
+        }
+        else return 'В комнате темно и нет людей.';
+    }
+}
+
